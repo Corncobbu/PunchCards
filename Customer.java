f84a9c8b542a499ae034d52c3db6c1432a74f51f
Connie Liou <32245011+Corncobbu@users.noreply.github.com>
2017-11-17 18:06:11 -0500

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robotics;
+
+/**
+ *
+ * @author connie.liou
+ */
+public class Customer {
+    
+    private String name;
+    private int saladPunches;
+    private int coffeePunches;
+   
+    public Customer(String n){
+        name =n;
+    }
+    public Customer (String n, int saladCount, int coffeeCount){
+        name = n;
+        saladPunches = saladCount;
+        coffeePunches = coffeeCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSaladPunchCount() {
+        return saladPunches;
+    }
+
+    public int getCoffeePunchCount() {
+        return coffeePunches;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void subtractSalad(int num){
+        saladPunches -= num;
+        System.out.println("New salad punches: "+saladPunches);
+    }
+    
+    public void subtractCoffee(int num){
+        coffeePunches -= num;
+        System.out.println("New coffee punches: "+coffeePunches);
+    }
+
+    public void addSaladPunch (int inc) {
+        saladPunches += inc;
+        if (saladPunches >=12){
+            saladPunches -= 12;
+            System.out.println(name + " has completed their salad card!");
+            System.out.println(name + " has " + saladPunches + " salad card punches.");
+        }
+        else{
+            System.out.println(name + " has " + saladPunches + " salad card punches.");
+        }
+    }
+    public void addCoffeePunch (int inc) {
+        coffeePunches += inc;
+        if (coffeePunches >=12){
+            coffeePunches -= 12;
+            System.out.println(name + " has completed their coffee card!");
+            System.out.println(name + " has " + coffeePunches + " coffee card punches.");
+        }
+        else{
+            System.out.println(name + " has " + coffeePunches + " coffee card punches.");
+        }
+    }
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Salad punch count: " + saladPunches + ", Coffee punch count: "+ coffeePunches;
+    }
+    
+    
+    
+}
