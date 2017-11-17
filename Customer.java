2f2d7222e5bb2c3b06602b09d72f465a73c5c1b1
Connie Liou <32245011+Corncobbu@users.noreply.github.com>
2017-11-17 18:07:05 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package robotics;
-
-/**
- *
- * @author connie.liou
- */
 public class Customer {
     
     private String name;
@@ -76,7 +65,5 @@ public class Customer {
     public String toString() {
         return "Name: " + name + ", Salad punch count: " + saladPunches + ", Coffee punch count: "+ coffeePunches;
     }
-    
-    
     
 }
